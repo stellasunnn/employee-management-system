638f123ef135024769d79f61d2a59b0bd5a552ca
Jing Yang <yljing422@gmail.com>
2025-04-27 11:09:40 -0700

    Resolved css config error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+// tailwind.config.js
+import animatePlugin from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -53,5 +56,7 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [
+    animatePlugin
+  ],
 };
